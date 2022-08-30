e2696a21441e417248e9a6c5cbf00174e42a2fd4
muradorucov <orucowmurad@gmail.com>
2022-08-30 06:40:22 +0400

    filter method

--- app.js
+++ app.js
@@ -15,6 +15,23 @@ console.log("Map Mehtod result: ", mappedNumbers); // [2, 4, 6]
 //some
 //every
 //filter
+Array.prototype.filterMethod = function (cb) {
+    const result = [];
+    const filterArr=[];
+
+    for (let i = 0; i < this.length; i++) {
+        result.push(cb(this[i], i, this));
+        if(cb(this[i])){
+            filterArr.push(this[i])
+        }    
+    }
+    return filterArr
+}
+
+const filterNumbers = [4, 2, 7, 8, 1, 3].filterMethod((num) => num < 5 )
+
+console.log("Filter Mehtod result: ", filterNumbers);
+
 //reduce
 //forEach
 Array.prototype.forEachMethod = function (cb) {
@@ -30,7 +47,7 @@ Array.prototype.forEachMethod = function (cb) {
 }
 
 const foreachNumbers = [1, 2, 3].forEachMethod((num) => {
-    console.log("ForEach Mehtod result: ", num)
+    console.log("ForEach Mehtod result: ", num)  // 1, 2, 3
 });
 
 //flat
